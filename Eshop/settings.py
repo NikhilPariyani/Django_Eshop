04297996cb57b6786457586e1b1c00219cee6013
NikhilPariyani <73129685+NikhilPariyani@users.noreply.github.com>
2020-12-06 19:21:21 +0530

    Update settings.py

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+Default : None
 STATIC_URL = '/static/'
 MEDIA_URL = "/images/myproduct/downloads/"
 MEDIA_ROOT = BASE_DIR
